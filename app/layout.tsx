af85d94622528b7b370d4714eda52bcfcab83eac
GabrielMCC2020 <gabrielcondori@upeu.edu.pe>
2026-01-27 12:43:06 -0500

    database setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 
 export const metadata: Metadata = {
@@ -21,8 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={outfit.className}
-      >
+      > 
+        <ConvexClientProvider>
         {children}
+        </ConvexClientProvider>
       </body>
     </html>
     </ClerkProvider>
